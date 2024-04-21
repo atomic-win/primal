44e90d53405bbc54d6fd747d1667a3f7d88b8d05
Manvendra <manvendra.kumar7@gmail.com>
2024-04-21 12:51:02 +0530

    Ignoring non-http uri schemes while adding site

--- src/Primal.Application/Sites/Commands/AddSite/AddSiteCommandHandler.cs
+++ src/Primal.Application/Sites/Commands/AddSite/AddSiteCommandHandler.cs
@@ -1,11 +1,18 @@
 using ErrorOr;
 using MediatR;
 using Primal.Application.Common.Interfaces.Persistence;
+using Primal.Domain.Sites;
 
 namespace Primal.Application.Sites;
 
 internal sealed class AddSiteCommandHandler : IRequestHandler<AddSiteCommand, ErrorOr<SiteResult>>
 {
+	private readonly HashSet<string> allowedUriSchemes = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+	{
+		"http",
+		"https",
+	};
+
 	private readonly ISiteRepository siteRepository;
 
 	public AddSiteCommandHandler(ISiteRepository sitesRepository)
@@ -15,6 +22,11 @@ internal sealed class AddSiteCommandHandler : IRequestHandler<AddSiteCommand, Er
 
 	public async Task<ErrorOr<SiteResult>> Handle(AddSiteCommand request, CancellationToken cancellationToken)
 	{
+		if (!this.allowedUriSchemes.Contains(request.Url.Scheme))
+		{
+			return new SiteResult(new SiteId(Guid.Empty), request.Url.Host, 0);
+		}
+
 		var errorOrSite = await this.siteRepository.AddSite(request.UserId, request.Url, request.DailyLimitInMinutes, cancellationToken);
 
 		return errorOrSite.Match(
