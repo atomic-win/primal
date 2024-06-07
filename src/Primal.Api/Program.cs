9dc83a41c75c553551f9cbbaf5110145f41afc2f
Manvendra <manvendra.kumar7@gmail.com>
2024-06-07 21:00:21 +0530

    chore: Add CORS policy for localhost:5173

--- src/Primal.Api/Program.cs
+++ src/Primal.Api/Program.cs
@@ -8,6 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 		.AddApplication()
 		.AddInfrastructure(builder.Configuration)
 		.AddPresentation();
+
+	builder.Services.AddCors(options =>
+	{
+		options.AddDefaultPolicy(
+			policy =>
+			{
+				policy.WithOrigins("http://localhost:5173")
+					.AllowAnyHeader()
+					.AllowAnyMethod();
+			});
+	});
 }
 
 var app = builder.Build();
@@ -15,6 +26,7 @@ var app = builder.Build();
 	app.UseHttpsRedirection();
 	app.UseAuthentication();
 	app.UseUserMiddleware();
+	app.UseCors();
 	app.UseAuthorization();
 	app.MapControllers();
 	app.Run();
