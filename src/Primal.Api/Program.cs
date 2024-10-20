953ac06b87083caeb0acef4d4a3f9a00747dcea8
Manvendra <manvendra.kumar7@gmail.com>
2024-10-20 08:40:20 +0530

    Update CORS policy to allow requests from "http://localhost:3000"

--- src/Primal.Api/Program.cs
+++ src/Primal.Api/Program.cs
@@ -27,7 +27,7 @@ var builder = WebApplication.CreateBuilder(args);
 		options.AddDefaultPolicy(
 			policy =>
 			{
-				policy.WithOrigins("http://localhost:5173")
+				policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
 					.AllowAnyHeader()
 					.AllowAnyMethod();
 			});
