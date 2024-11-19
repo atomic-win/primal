e4783c8714015fd82c6078033a64c3585f9cbf21
Manvendra <manvendra.kumar7@gmail.com>
2024-11-19 21:24:43 +0530

    Add asset repository dependency and error handling in GetTransactionsByAssetIdQueryHandler

--- src/Primal.Application/Investments/Queries/GetTransactionsByAssetId/GetTransactionsByAssetIdQueryHandler.cs
+++ src/Primal.Application/Investments/Queries/GetTransactionsByAssetId/GetTransactionsByAssetIdQueryHandler.cs
@@ -6,19 +6,32 @@ namespace Primal.Application.Investments;
 
 internal sealed class GetTransactionsByAssetIdQueryHandler : IRequestHandler<GetTransactionsByAssetIdQuery, ErrorOr<IEnumerable<TransactionResult>>>
 {
+	private readonly IAssetRepository assetRepository;
 	private readonly ITransactionRepository transactionRepository;
 	private readonly InvestmentCalculator investmentCalculator;
 
 	public GetTransactionsByAssetIdQueryHandler(
+		IAssetRepository assetRepository,
 		ITransactionRepository transactionRepository,
 		InvestmentCalculator investmentCalculator)
 	{
+		this.assetRepository = assetRepository;
 		this.transactionRepository = transactionRepository;
 		this.investmentCalculator = investmentCalculator;
 	}
 
 	public async Task<ErrorOr<IEnumerable<TransactionResult>>> Handle(GetTransactionsByAssetIdQuery request, CancellationToken cancellationToken)
 	{
+		var errorOrAsset = await this.assetRepository.GetByIdAsync(
+			request.UserId,
+			request.AssetId,
+			cancellationToken);
+
+		if (errorOrAsset.IsError)
+		{
+			return errorOrAsset.Errors;
+		}
+
 		var errorOrTransactions = await this.transactionRepository.GetByAssetIdAsync(
 			request.UserId,
 			request.AssetId,
